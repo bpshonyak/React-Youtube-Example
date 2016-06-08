4e720bee05bdd254bde05eb83524a9871ad901b0
Bogdan Pshonyak <bpshonyak@live.com>
2016-06-08 16:08:38 -0700

    updates to the search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,27 @@
 // const Component = React.Component;
 import React, { Component } from 'react';
 
-// Clas based component
+// Class based component [used when 'state' is needed]
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    // Initailize state object
+    this.state = { term: 'Default Value' };
+  }
   render() {
-    return <input />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+      </div>
+    );
+  }
+  onInputChange = (event) => {
+    this.setState({
+      term: event.target.value
+    });
   }
 }
 
