2e16c518d13605f2d3537dd3faaecc856011c64a
Bogdan Pshonyak <bpshonyak@live.com>
2016-06-08 16:19:57 -0700

    youtube api works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Import React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+// Youtube search API
+import YTSearch from 'youtube-api-search';
 
 // Import components
 import SearchBar from './components/search_bar';
@@ -10,12 +13,27 @@ const API_KEY = "AIzaSyACpKIVaL4UeXbKhagm3PKaViUwkSh-yhM";
 
 // Create a new component. This compenent shold
 // prodce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+
+    // Fetch inital YouTube videos
+    YTSearch({ key: API_KEY, term: 'sol' }, function(data){
+      console.log(data);
+    });
+
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Render an App component instance.
